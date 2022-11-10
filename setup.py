e9c1b902e4f489a8d2946be61826eb6c40650a6a
jolsten <jolsten@gmail.com>
2022-11-10 15:44:02 -0500

    cleaned it up a bit, added .gitignore

--- setup.py
+++ setup.py
@@ -1,25 +1,22 @@
 """
-To understand this project's build structure
+This project's build structure
  - This project uses setuptools, so it is declared as the build system in the pyproject.toml file
- - We use as much as possible `setup.cfg` to store the information
+ - We use as much as possible `setup.cfg` to store the information so that we can get some mandatory
+   metadata in PKG-INFO that isn't possible with the pyproject.toml approach
 See also:
   https://setuptools.readthedocs.io/en/latest/setuptools.html#configuring-setup-using-setup-cfg-files
+  https://stackoverflow.com/questions/73279327/how-to-indicate-use-scm-version-in-setup-cf
+  https://github.com/smarie/python-genbadge
 """
 from setuptools import setup
-
-# (1) read the setup.cfg to grab useful metadata
 from setuptools.config import read_configuration
+
 conf_dict = read_configuration("setup.cfg")
 PKG_NAME = conf_dict['metadata']['name']
 
-
-# (2) Call setup() with as little args as possible
-# Unfortunately, currently the only way to use setuptools_scm and get "author" and "maintainer" metadata in 
-# PKG-INFO is to employ the use_scm_version here instead of in setup.cfg
-# See https://stackoverflow.com/questions/73279327/how-to-indicate-use-scm-version-in-setup-cfg for solution
-# used in https://github.com/smarie/python-genbadge
 setup(
+    # we can't put `use_scm_version` in setup.cfg yet unfortunately
     use_scm_version={
-        "write_to": "src/%s/_version.py" % PKG_NAME
-    },  # we can't put `use_scm_version` in setup.cfg yet unfortunately
+        "write_to": "src/%s/_version.py" % PKG_NAME  
+    },  
 )
